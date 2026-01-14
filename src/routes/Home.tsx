e671fb0ea3585082be481df27c60733fb3c3c1f7
Park Junyoung <bloomwayz@snu.ac.kr>
2026-01-15 06:29:43 +0900

    💄 Add a landing page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,7 @@
 import EventCardView from '@/components/EventCardView';
+import { Button } from '@/components/ui/button';
 import useAuth from '@/hooks/useAuth';
+import { Link } from 'react-router';
 
 const events = [
   {
@@ -41,20 +43,40 @@ const events = [
 export default function Home() {
   const { isLoggedIn } = useAuth();
 
+  // if logged in, show the landing page
+  if (!isLoggedIn) {
+    return (
+      <div className="flex-1 flex items-center justify-center">
+        <div className="flex w-full max-w-md flex-col gap-10">
+          <h1 className="text-4xl font-bold text-center animate-[wiggle_1s_ease-in-out_infinite]">
+            복잡한 모임 관리, <br></br> 모이샤로 쉽게!
+          </h1>
+          <div className="flex flex-col items-center justify-center gap-4">
+            <Link to="/register">
+              <Button className="bg-blue-600 hover:bg-blue-700 text-lg font-semibold px-25 py-5">
+                계정 만들기
+              </Button>
+            </Link>
+            <Link to="/login">
+              <Button
+                variant="outline"
+                className="text-lg font-semibold px-25 py-5"
+              >
+                로그인하기
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  // otherwise, show the dashboard with event cards
   return (
     <div className="flex-1 flex justify-center">
-      {!isLoggedIn ? (
-        <div className="flex w-full max-w-md flex-col gap-6">
-          <EventCardView events={events} />
-        </div>
-      ) : (
-        <div className="text-center">
-          {/* TODO: Implement the landing page */}
-          <h1 className="text-4xl font-bold text-gray-900 mb-4">
-            Welcome to Moisha
-          </h1>
-        </div>
-      )}
+      <div className="flex w-full max-w-md flex-col gap-6">
+        <EventCardView events={events} />
+      </div>
     </div>
   );
 }

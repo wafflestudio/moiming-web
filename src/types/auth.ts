d48ee9e41158fb1cf165861e6211c7176815e4a3
Park Junyoung <bloomwayz@snu.ac.kr>
2026-02-04 18:43:13 +0900

    👽️ Google available only

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -33,7 +33,7 @@ export interface LogoutResponse {
 
 // ---------- /social ----------
 
-type AuthProvider = 'GOOGLE';
+export type AuthProvider = 'GOOGLE';
 
 export interface SocialLoginRequest {
   provider: AuthProvider;

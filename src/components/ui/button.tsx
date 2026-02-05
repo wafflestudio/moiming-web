c43b69deddac96a517c876a9f203a2d0e157b29f
jun-0411 <jun411@snu.ac.kr>
2026-02-05 23:16:32 +0900

    💄Udpate global css

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,6 +10,10 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        moiming:
+          'bg-primary text-white shadow-md hover:bg-primary/90 font-bold rounded-2xl',
+        moimingOutline:
+          'border-2 border-primary text-primary bg-white hover:bg-blue-50 font-bold rounded-2xl',
         destructive:
           'bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:

c81ea0e8a58a6dfae287dc4512f59e7d41c3f17f
jun-0411 <jun411@snu.ac.kr>
2026-02-07 10:23:20 +0900

    🔥Remove kakao auth url

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -7,6 +7,6 @@ export const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?cli
 }&redirect_uri=${REDIRECT_URI_BASE}/google&response_type=code&scope=openid email profile`;
 
 // 카카오 인증 URL
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${
-  import.meta.env.VITE_KAKAO_CLIENT_ID
-}&redirect_uri=${REDIRECT_URI_BASE}/kakao&response_type=code`;
+// export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${
+//   import.meta.env.VITE_KAKAO_CLIENT_ID
+// }&redirect_uri=${REDIRECT_URI_BASE}/kakao&response_type=code`;

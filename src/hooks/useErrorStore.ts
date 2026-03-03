fe7b3c399820d35bd1dc69e48a85a503c6e5e08f
jun-0411 <jun411@snu.ac.kr>
2026-03-03 16:32:00 +0900

    💄Make modal closing delay to 100ms & add confirm button text

--- src/hooks/useErrorStore.ts
+++ src/hooks/useErrorStore.ts
@@ -4,8 +4,14 @@ interface ErrorState {
   isOpen: boolean;
   title: string;
   message: string;
+  confirmText: string;
   onConfirm?: () => void; // 확인 버튼 클릭 시 실행할 콜백 함수
-  showError: (message: string, title?: string, onConfirm?: () => void) => void;
+  showError: (
+    message: string,
+    title?: string,
+    onConfirm?: () => void,
+    confirmText?: string
+  ) => void;
   closeError: () => void;
 }
 
@@ -13,8 +19,15 @@ export const useErrorStore = create<ErrorState>((set) => ({
   isOpen: false,
   title: '오류 발생',
   message: '',
+  confirmText: '확인',
   onConfirm: undefined,
-  showError: (message, title = '오류 발생', onConfirm) =>
-    set({ isOpen: true, message, title, onConfirm }),
-  closeError: () => set({ isOpen: false, message: '', onConfirm: undefined }),
+  showError: (
+    message,
+    title = '오류 발생',
+    onConfirm,
+    confirmText = '확인'
+  ) => {
+    set({ isOpen: true, message, title, onConfirm, confirmText });
+  },
+  closeError: () => set({ isOpen: false }), // 리다이렉트 중 깜빡임 방지를 위해 내용 보존
 }));

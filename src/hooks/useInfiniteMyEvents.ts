3117a41b98d8e856399e8b8cfb68de98c13224d5
jun-0411 <jun411@snu.ac.kr>
2026-03-17 00:29:24 +0900

    ✨ Add hook for infinite scroll

---
+++ src/hooks/useInfiniteMyEvents.ts
@@ -0,0 +1,20 @@
+import getMyEvents from '@/api/events/me';
+import type { MyEventsResponse } from '@/types/events';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+export default function useInfiniteMyEvents() {
+  return useInfiniteQuery<MyEventsResponse, Error>({
+    queryKey: ['myEvents'],
+    queryFn: async ({ pageParam = undefined }) => {
+      const { data } = await getMyEvents(pageParam as string | undefined);
+      return data;
+    },
+    getNextPageParam: (lastPage) => {
+      if (lastPage.hasNext) {
+        return lastPage.nextCursor;
+      }
+      return undefined;
+    },
+    initialPageParam: undefined,
+  });
+}

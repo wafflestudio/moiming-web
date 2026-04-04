5f856ecfe92e181efc30b47589ea6ff2669d97c4
Park Junyoung <bloomwayz@snu.ac.kr>
2026-04-04 17:08:00 +0900

    ♿️ Auto-focus on modals

--- src/components/GlobalErrorModal.tsx
+++ src/components/GlobalErrorModal.tsx
@@ -44,7 +44,7 @@ export function GlobalErrorModal() {
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
-          <AlertDialogAction onClick={handleConfirm}>
+          <AlertDialogAction onClick={handleConfirm} autoFocus>
             {confirmText}
           </AlertDialogAction>
         </AlertDialogFooter>
